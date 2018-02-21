b3986d23d803009ee42986a4cc79731b2f7174ec
Sergey Zylevich <sergey496@yandex.ru>
2018-02-21 20:28:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   if ((str.length%2)==1){return false;
+	
+	}
+for (var i=0; i<str.length;i++)
+	{
+		for (var j=0; j<bracketsConfig.length;j++)
+		{
+			if(str[i]==bracketsConfig[j][0])
+						{	
+							str=str.replace(bracketsConfig[j][0], "_");
+							var n=str.indexOf(bracketsConfig[j][1]);
+
+							if((n==-1)||(n<i)  )
+								{
+									return false;
+								}
+
+							str=str.replace(bracketsConfig[j][1], "_");						
+						}
+			}
+
+	}
+	return true;
 }
